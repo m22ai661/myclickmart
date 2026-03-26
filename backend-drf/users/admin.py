526d0e39b34007ff17ddb56f1effcd6c5c4d86f1
m22ai661 <m22ai661@alumni.iitj.ac.in>
2026-03-26 22:30:58 +0530

    First Commit

---
+++ backend-drf/users/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, UserAdmin)
